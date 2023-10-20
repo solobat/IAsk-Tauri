91d402ad76b7fa9acbc9c652c13add0bdfb04374
solobat <solopea@icloud.com>
2023-10-20 15:45:59 +0800

    feat: add cache for marked

--- src/helpers/marked.helper.ts
+++ src/helpers/marked.helper.ts
@@ -1,6 +1,7 @@
 import { marked } from "marked";
 import hljs from "./highlight.helper";
 import TaskRender from "../extensions/marked.task";
+import { LRUCache } from "lru-cache";
 
 marked.setOptions({
   highlight: function (code, language) {
@@ -12,11 +13,21 @@ marked.setOptions({
 
 marked.use({ renderer: TaskRender });
 
+const cache = new LRUCache({
+  max: 500,
+});
 export function parseMarkdown(content = "") {
   const text = content.trim();
 
   if (text) {
-    return marked(text);
+    if (cache.has(text)) {
+      return cache.get(text);
+    } else {
+      const html = marked(text);
+      cache.set(text, html);
+
+      return html;
+    }
   } else {
     return "";
   }
